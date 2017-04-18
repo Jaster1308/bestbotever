03f2f082e726478e2d6ed2d24b803f3a78f5a10e
Haruki <harukixsira@gmail.com>
2017-04-18 17:53:12 -0500

    editing

--- bot.js
+++ bot.js
@@ -1,13 +1,13 @@
-var Twit = require('twit');
+var twit = require('twit');
 var TwitterBot = require('node-twitterbot').TwitterBot;
 
 // Want the twit bot to reply to the user so I have to make it active
 
-var stream = Twitter.stream('user');
+// var stream = Twitter.stream('user');
 
 // This should follow the person that follows the bot
 
-stream.on('follow', followed);
+// stream.on('follow', followed);
 
 var Bot = new TwitterBot({
     consumer_key: process.env.BOT_CONSUMER_KEY,
@@ -45,32 +45,33 @@ function chooseRandom(myArray) {
     return myArray[Math.floor(Math.random() * myArray.length)];
 }
 // Function for replying to the user who followed the bot
-function followed(event){
-    console.log('Follow Event is running.....');
-    // Gotta grab that good ole twitter handle
-
-    var
-        name = event.source.name;
-        screenName = event.source.screen_name;
-    // This should reply to with a sentence
-    tweetNow('@'+ screenName + ' What the heck are you following me for?')
-}
-
-// this is validation to see if the tweet go to the user who followed
-function tweetNow(tweetTxt) {
-    var tweet = {
-        status: tweetTxt
-    };
-    Twitter.post('statuses/updates', tweet, function(err, data, response){
-        if(err){
-            console.log("There's something wrong here......")
-        }
-        else{
-            console.log('Lots of Love!')
-        }
-    });
-}
+// function followed(event){
+//     console.log('Follow Event is running.....');
+//     // Gotta grab that good ole twitter handle
+//
+//     var
+//         name = event.source.name;
+//         screenName = event.source.screen_name;
+//     // This should reply to with a sentence
+//     tweetNow('@'+ screenName + ' What the heck are you following me for?')
+// }
+//
+// // this is validation to see if the tweet go to the user who followed
+// function tweetNow(tweetTxt) {
+//     var tweet = {
+//         status: tweetTxt
+//     };
+//     Twitter.post('statuses/updates', tweet, function(err, data, response){
+//         if(err){
+//             console.log("There's something wrong here......")
+//         }
+//         else{
+//             console.log('Lots of Love!')
+//         }
+//     });
+// }
 
 var phrase = chooseRandom(phraseArray) + "Today my " + chooseRandom(japNouns) + chooseRandom(japEnds) +
     "told me about " + chooseRandom(japManga) + "."+ " Should I read it?";
+console.log(phrase);
 Bot.tweet(phrase);
