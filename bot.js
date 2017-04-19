80dcb27b5787d49abc95e19938ea83a05cb002a1
Haruki <harukixsira@gmail.com>
2017-04-19 13:41:00 -0500

    made more sentences and variables for the bot to choose from

--- bot.js
+++ bot.js
@@ -34,35 +34,74 @@ var japManga = ["Bleach", "Naruto", "Boruto", "One Piece", "Shippuden",
 //Honorifics cuz the bot has to get really japanese with it
 var japEnds = ["-san", "-chan", "-chi", "-sama", "-kun", "-bo"];
 
+var japFood = ["Ramen", "Soba", "Tempura", "Yakitori", "Sushi",
+                "Udon", "Sashimi", "Onigiri", "Miso Soup", "Natto",
+                "Sukiyaki", "Takoyaki", "Mochi", "Tofu", "Edamame"];
+
+var japPlaces = ["Mount Fuji", "Kinkaku-ji", "Meiji Shrine", "Shinjuku Gyoen",
+                    "Itsukushima", "Roppongi", "Odaiba", "Ueno Park", "Tokyo National Museum",
+                    "Yoyogi Park", "Arcade"];
+
+var japCultures = ["Kawaii stuff", "Japanese idols", "Manga", "Anime", "Fashion", "Films", "Video Games"];
+
+var reactions = ["good", "awesome", "impeccable", "ugly", "gross", "awful", "very delicious"];
+
 function chooseRandom(myArray) {
     return myArray[Math.floor(Math.random() * myArray.length)];
 }
-// Want the twit bot to reply to the user so I have to make it active
 
-var Twitter = new Tweetpackage(Bot);
+// function mySentences() {
+//     return chooseRandom(styles)
+// }
+styles = [family, todo, food];
 
-// This should follow the person that follows the bot
+selectedStyle = chooseRandom(styles);
 
-stream.on('follow', followed);
+if (styles = family){
+    var phrase = chooseRandom(phraseArray) + " Today my " + chooseRandom(japNouns) + chooseRandom(japEnds) +
+        " told me about " + chooseRandom(japManga) + "."+ " Should I read it?";
+    console.log(phrase);
+    Bot.tweet(phrase);
+}else if (styles = todo){
+    var things = "Today I am going to " + chooseRandom(japPlaces) + " to look at " +
+         + chooseRandom(japCultures) + "." + "What else should I do here?!";
+    console.log(things);
+    Bot.tweet(things);
+}else if (styles = food) {
+    var eating = "At " + chooseRandom(japPlaces) + " I tried " + chooseRandom(japFood) +
+            ", it was " + chooseRandom(reactions) + '.';
+    console.log(eating);
+    Bot.tweet(eating)
+}
 
-Twitter.stream('statuses/filter', {track: '#animebot'}, function(stream) {
-    stream.on('data', function(tweet) {
-        console.log(tweet.text);
-        var reply = {status: "Yo @"+ tweet.user.screen_name + ", What do you want?"};
-        Twitter.post('statuses/update', reply, function(error, tweetReply, response){
-            if(error){
-                console.log(error);
-            }
-            console.log(tweetReply.text);
-        });
-    });
-    stream.on('error', function(error) {
-        console.log(error);
-    });
-});
+// console.log(mySentences());
+// Bot.tweet(mySentences());
 
 
 
+// // Want the twit bot to reply to the user so I have to make it active
+//
+// var Twitter = new Tweetpackage(Bot);
+//
+// // This should follow the person that follows the bot
+//
+// stream.on('follow', followed);
+//
+// Twitter.stream('statuses/filter', {track: '#animebot'}, function(stream) {
+//     stream.on('data', function(tweet) {
+//         console.log(tweet.text);
+//         var reply = {status: "Yo @"+ tweet.user.screen_name + ", What do you want?"};
+//         Twitter.post('statuses/update', reply, function(error, tweetReply, response){
+//             if(error){
+//                 console.log(error);
+//             }
+//             console.log(tweetReply.text);
+//         });
+//     });
+//     stream.on('error', function(error) {
+//         console.log(error);
+//     });
+// });
 // Function for replying to the user who followed the bot
 // function followed(event){
 //     console.log('Follow Event is running.....');
@@ -89,8 +128,3 @@ Twitter.stream('statuses/filter', {track: '#animebot'}, function(stream) {
 //         }
 //     });
 // }
-
-var phrase = chooseRandom(phraseArray) + " Today my " + chooseRandom(japNouns) + chooseRandom(japEnds) +
-    " told me about " + chooseRandom(japManga) + "."+ " Should I read it?";
-console.log(phrase);
-Bot.tweet(phrase);
